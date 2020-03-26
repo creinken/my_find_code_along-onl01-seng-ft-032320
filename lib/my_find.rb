001b405c89d8f78092ee65e591dbcf986f3ab510
Christopher Reinken <christopherreinken@gmail.com>
2020-03-26 15:33:01 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,10 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-    yield(collection[i])
-    i = i + 1
+    if yield(collection[i])
+      return collection[i]
+    else
+      i = i + 1
+    end
   end
 end
